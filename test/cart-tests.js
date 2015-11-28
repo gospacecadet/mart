c20e27485c5c666b46b9eb3342c90d5f11c2113f
Marvin Arnold <marvinmarnold@gmail.com>
2015-11-27 21:03:01 -0600

    Clean up Cart/LineItem helpers

--- test/cart-tests.js
+++ test/cart-tests.js
@@ -1,38 +1,38 @@
 if(Meteor.isServer) {
-  var cartId = Mart.Carts.insert({userId: "testId"}, {validate: false}) // can't login users easily
-  var cart = Mart.Carts.findOne(cartId)
-
-  var storefrontId1 = Mart.Storefronts.insert({
-    name: "Test Storefront",
-    description: "A test storefront description",
-    isActive: true,
-    userId: "testId"
-  }, {validate: false})
-
-  var storefrontId2 = Mart.Storefronts.insert({
-    name: "Test Storefront2",
-    description: "A test storefront description2",
-    isActive: true,
-    userId: "testId2"
-  }, {validate: false})
-
-  var productId1 = Mart.Products.insert({
-    name: "test product",
-    description: "a test description",
-    unitPrice: 5.23,
-    storefrontId: storefrontId1,
-    isActive: true
-  })
-
-  var productId2 = Mart.Products.insert({
-    name: "test product2",
-    description: "a test description2",
-    unitPrice: 1098.92,
-    storefrontId: storefrontId2,
-    isActive: true
-  })
-
   Tinytest.add('Cart - has a subtotal', function (test) {
+    var cartId = Mart.Carts.insert({userId: "testId"}, {validate: false}) // can't login users easily
+    var cart = Mart.Carts.findOne(cartId)
+
+    var storefrontId1 = Mart.Storefronts.insert({
+      name: "Test Storefront",
+      description: "A test storefront description",
+      isActive: true,
+      userId: "testId"
+    }, {validate: false})
+
+    var storefrontId2 = Mart.Storefronts.insert({
+      name: "Test Storefront2",
+      description: "A test storefront description2",
+      isActive: true,
+      userId: "testId2"
+    }, {validate: false})
+
+    var productId1 = Mart.Products.insert({
+      name: "test product",
+      description: "a test description",
+      unitPrice: 5.23,
+      storefrontId: storefrontId1,
+      isActive: true
+    })
+
+    var productId2 = Mart.Products.insert({
+      name: "test product2",
+      description: "a test description2",
+      unitPrice: 1098.92,
+      storefrontId: storefrontId2,
+      isActive: true
+    })
+    
     Mart.LineItems.insert({productId: productId1, quantity: 3, cartId: cartId})
     Mart.LineItems.insert({productId: productId2, quantity: 42, cartId: cartId})
 
